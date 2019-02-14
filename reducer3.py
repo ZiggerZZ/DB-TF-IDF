00497775ca320c283bf4db8d2f25bf8b6c07808b
ranilovi <dora.ranilovic@mail.utoronto.ca>
2019-02-14 21:07:18 +0100

    mapreduce scripts

---
+++ reducer3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+###########################################  reducer3.py
+
+### Reduce
+# Input:((term,docname), tfidf)
+# Output: ((term,docname), tfidf)
+
+
+import sys
+
+
+for line in sys.stdin:
+    print (line)
