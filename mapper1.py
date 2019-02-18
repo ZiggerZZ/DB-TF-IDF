854feabe0b89248b4145668fe197784d3f95f894
Roberta Conrad <rjcnrd@gmail.com>
2019-02-18 11:07:21 +0100

    correct input path

--- mapper1.py
+++ mapper1.py
@@ -5,13 +5,13 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     terms = line.split(" ")
-    # path = os.environ['mapreduce_map_input_file'].split("/")
+    path = os.environ['mapreduce_map_input_file'].split("/")
     # path = "C:/Users/dorar_000/Documents/
     # GitHub/DB-TF-IDF/data/Aladdin.txt".split('/')
-    path = '''/Users/robertaconrad/Documents/
-                07_Ecole_Polytechnique_Studienunterlagen/
-                Data_Base_Management/DB-TF-IDF/data/Aladdin.txt'''.split(
-        '/')
+    # path = '''/Users/robertaconrad/Documents/
+    #            07_Ecole_Polytechnique_Studienunterlagen/
+    #           Data_Base_Management/DB-TF-IDF/data/Aladdin.txt'''.split(
+    #  '/')
     docname = path[-1]
     for term in terms:
         # term = term.strip('''!()-[]{};:'"\,<>./?@#$%^&*_~''').lower()
