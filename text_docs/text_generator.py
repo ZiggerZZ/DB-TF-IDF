a3de018bfe87a12c4696d519726a0b9a60011f88
Roberta Conrad <rjcnrd@gmail.com>
2019-02-19 17:23:36 +0100

    added startnr

--- text_docs/text_generator.py
+++ text_docs/text_generator.py
@@ -4,7 +4,7 @@ import random
 corpus_length = len(brown.words())
 
 
-def create_docs(number_of_words_per_doc=200, num_doc=10):
+def create_docs(number_of_words_per_doc=200, num_doc=10,startnr = 0):
     # control number of words per doc
     # and number of documents
     # we fix the line length at 20
@@ -14,7 +14,7 @@ def create_docs(number_of_words_per_doc=200, num_doc=10):
     for i in range(0, num_doc):
         # create new file with writing + permission
         new_file = open("textdoc"+str(number_of_words_per_doc) +
-                        "words" + str(i)+".txt", "w+")
+                        "words" + str(i + startnr)+".txt", "w+")
         for line in range(0, number_of_lines):
             index = random.randint(1, corpus_length)
             words = brown.words()[index: index+line_length]
