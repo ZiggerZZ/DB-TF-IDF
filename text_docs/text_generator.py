1f4111187b9587e5a9540c13b2bc2bba188819be
Roberta Conrad <rjcnrd@gmail.com>
2019-02-19 16:01:25 +0100

    added script

--- text_docs/text_generator.py
+++ text_docs/text_generator.py
@@ -3,22 +3,22 @@ from nltk.corpus import brown
 import random
 corpus_length = len(brown.words())
 
-# control number of words per doc
-number_of_words_per_doc = 200
-# and number of documents
-num_doc = 10
 
-#we fix the line length at 20
-line_length = 20
-number_of_lines = int(number_of_words_per_doc / line_length)
+def create_docs(number_of_words_per_doc=200, num_doc=10):
+    # control number of words per doc
+    # and number of documents
+    # we fix the line length at 20
+    line_length = 20
+    number_of_lines = int(number_of_words_per_doc / line_length)
 
-for i in range(0, num_doc):
-    #create new file with writing + permission
-    new_file = open("textdoc"+str(i)+".txt", "w+")
-    for line in range(0, number_of_lines):
-        index = random.randint(1, corpus_length)
-        words = brown.words()[index: index+20]
-        sentence = ' '.join(word for word in words)
-        new_file.write(sentence + "\n")
+    for i in range(0, num_doc):
+        # create new file with writing + permission
+        new_file = open("textdoc"+str(number_of_words_per_doc) +
+                        "words" + str(i)+".txt", "w+")
+        for line in range(0, number_of_lines):
+            index = random.randint(1, corpus_length)
+            words = brown.words()[index: index+line_length]
+            sentence = ' '.join(word for word in words)
+            new_file.write(sentence + "\n")
 
-    new_file.close()
+        new_file.close()
